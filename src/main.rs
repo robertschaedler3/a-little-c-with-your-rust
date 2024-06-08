3d650f1fd28b067e21fc7df3313eb85386368c93
Robert Schaedler <rschaedler@microsoft.com>
2024-06-08 06:03:18 +0000

    compile and link to lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use rust_plus_c::say_hello;
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::env::args().nth(1).unwrap_or("Rust".to_string());
+    let name = std::ffi::CString::new(input).unwrap();
+
+    unsafe {
+        say_hello(name.as_ptr());
+    }
 }
